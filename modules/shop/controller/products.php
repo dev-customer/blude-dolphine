69d3692170e2c80855de7e8a4c61aa91de5d9e7f
Thinhpm <phamminhthinh9x@gmail.com>
2018-08-11 17:52:24 +0700

    brand

--- modules/shop/controller/products.php
+++ modules/shop/controller/products.php
@@ -36,14 +36,85 @@ class Products extends Module{
 			}
 				
 		} 				
+		//filter manuafct
+		if($_GET['malias'] != '')
+		{
+			$man = $this->loadObject('SELECT * FROM #__shop_manuafact WHERE alias = "'.$_GET['malias'].'"');  
+			if(!mpty($man)){	
+				$sqlExt .= " AND m.alias = '".$_GET['malias']."'"; 
+				$url = LINK_SHOP_MANUFACT_ITEM.$_GET['malias'].'.html';
+			} 
+		} 
+		//filter location
+		//if($_POST['sLocation'] != '')
+		//{ 
+		//	$sqlExt .= " AND c.jsLocationGroup LIKE '%".$_POST['sLocation']."%'";	 
+		//} 				
+		
+		$key = isset($_POST['keyword'])? addslashes($_POST['keyword']) : '';
+		$sqlExt .= !empty($key)? " AND  p.title LIKE '%".$key."%'" : '';
+		$sqlExt .= !empty($_POST['categorySearch'])? " AND p.id_cat = ".(int)$_POST['categorySearch'] : '';
+		
+		//echo $sqlExt;
+		
+		$data = loadListShop($fieldList, $sqlExt, '', $limit); 
+		$data['category'] = $db->loadObject('SELECT title FROM #__shop_category WHERE alias = "'.$_GET['alias'].'"');
+		
+		$url = $url !='' ? $url : LINK_SHOP_ALL;		
+		$data['paging'] = PagingFront::load($getpage, $data['totalRecord'], $rowpage, $curpage, $url);
+
+         
+         if(empty($man)) {
+			 $cate = $this->loadObject('SELECT * FROM #__shop_category WHERE alias = "'.$_GET['alias'].'"'); 
+			 $cShopSub = getCategoryShop($cate['id_cat'], '', '');  
+			 $data['list_category_child'] = $cShopSub;
+			 $data['id_main_cat'] = $cate['id_cat'];
+		}
+		$this->view('shop/view/products', $data);	
+	}
+
+	public function brand(){
+		global $db, $mod;	
+		$rowpage = 10;
+		$curpage = CUR_ROWS;
+		$getpage = empty($_GET['page']) ? 1 : $_GET['page']; 
+		$offset = ($getpage - 1) * $rowpage;	
+		$limit = $offset.','.$rowpage;
+
+		$data = [];
+						
+		$fieldList = " p.*, c.title AS category";
+		$url = ''; 
+		$sqlExt = " AND p.publish = 1";  
+		
+		if($_GET['alias'] != '')
+		{
+			if($_GET['alias'] == 'sale-off'){
+				$sqlExt .= " AND p.discount > 0"; 
+				$url = BASE_NAME.'sale-off.html';
+			}else{
+				$cate = $this->loadObject('SELECT * FROM #__shop_category WHERE alias = "'.$_GET['alias'].'"'); 
+				if(!empty($cate)){
+					$cListID = getCategoryShop($cate['id_cat'], '', '');
+					$cList 	 = categoryToArray($cListID); 
+					$cList[] = $cate['id_cat'];
+					$sqlExt .= " AND p.id_cat IN (".implode(',', $cList).")";
+					$url = LINK_SHOP_LIST.$_GET['alias'].'.html';					
+				}else{
+					$this->redirect(BASE_NAME); 
+				}
+			}
+				
+		} 				
 
 		//filter manuafct
 		if($_GET['malias'] != '')
 		{
 			$man = $this->loadObject('SELECT * FROM #__shop_manuafact WHERE alias = "'.$_GET['malias'].'"');  
-			if(!empty($man)){			
+			if(!empty($man)){
 				$sqlExt .= " AND m.alias = '".$_GET['malias']."'"; 
 				$url = LINK_SHOP_MANUFACT_ITEM.$_GET['malias'].'.html';
+				$brandName = $man['name'];
 			} 
 		} 				
 
@@ -59,20 +130,16 @@ class Products extends Module{
 		$sqlExt .= !empty($key)? " AND  p.title LIKE '%".$key."%'" : '';
 		$sqlExt .= !empty($_POST['categorySearch'])? " AND p.id_cat = ".(int)$_POST['categorySearch'] : '';
 		
-		//echo $sqlExt;
+		// echo $sqlExt;
 		
+		// die;
 		$data = loadListShop($fieldList, $sqlExt, '', $limit); 
 		$data['category'] = $db->loadObject('SELECT title FROM #__shop_category WHERE alias = "'.$_GET['alias'].'"');
 		
 		$url = $url !='' ? $url : LINK_SHOP_ALL;		
 		$data['paging'] = PagingFront::load($getpage, $data['totalRecord'], $rowpage, $curpage, $url);
-
-		$cate = $this->loadObject('SELECT * FROM #__shop_category WHERE alias = "'.$_GET['alias'].'"'); 
-		$cShopSub = getCategoryShop($cate['id_cat'], '', '');  
-		$data['list_category_child'] = $cShopSub;
-		$data['id_main_cat'] = $cate['id_cat'];
-
-		$this->view('shop/view/products', $data);	
+		$data['brandName'] = $brandName;
+		$this->view('shop/view/productsBrand', $data);	 
 	}
 		    
  
